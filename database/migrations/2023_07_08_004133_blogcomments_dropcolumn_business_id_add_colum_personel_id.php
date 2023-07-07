6c6cc80e1f9c6c30480e1f07c406cb8da0e35382
migrain.code <migrain.code@gmail.com>
2023-07-08 01:06:03 +0300

    blog yorumları işletme id kaldırıldı müşteri id eklendi

---
+++ database/migrations/2023_07_08_004133_blogcomments_dropcolumn_business_id_add_colum_personel_id.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog_comments', function (Blueprint $table){
+            $table->dropColumn('business_id');
+            $table->integer('customer_id')->after('blog_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
