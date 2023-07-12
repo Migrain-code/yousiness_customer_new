17422a7fe3d4abdcd348b1273e1227b145d4345c
migrain.code <migrain.code@gmail.com>
2023-07-12 18:40:58 +0300

    AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -50,8 +51,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind('Illuminate\Routing\ResourceRegistrar', function () use ($registrar) {
             return $registrar;
         });
-
-        if (App::environment('web')){
+        $request = Request::instance();
+        if (!$request->is('api/*')){
             $settings = [];
 
             foreach (Setting::all() as $item) {
