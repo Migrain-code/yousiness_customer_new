cce852de3b5b4c0bc3ce34e20d927ad47f48f224
migrain.code <migrain.code@gmail.com>
2023-07-29 00:00:49 +0300

    deneme

--- app/Http/Controllers/Api/Customer/AuthController.php
+++ app/Http/Controllers/Api/Customer/AuthController.php
@@ -60,7 +60,9 @@ class AuthController extends Controller
 
     public function existPhone($phone)
     {
+
         $existPhone = \App\Models\Customer::where('email', $phone)->first();
+        dd($existPhone);
         if ($existPhone != null) {
             $result = true;
         } else {
