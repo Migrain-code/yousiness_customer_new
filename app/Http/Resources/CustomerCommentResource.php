826da0c34874e87085eb8549c50176c109117db0
migrain.code <migrain.code@gmail.com>
2023-07-31 23:18:39 +0300

    müşterinin randevu yorumu listesi

--- app/Http/Resources/CustomerCommentResource.php
+++ app/Http/Resources/CustomerCommentResource.php
@@ -17,7 +17,8 @@ class CustomerCommentResource extends JsonResource
         return[
             'content' => $this->content,
             'point' => $this->point,
-            'created_at' => $this->created_at->format('d.m.Y H:i'),
+            'created_at' => $this->created_at->format('d.m.Y'),
+            'created_at_time' => $this->created_at->format('H:i'),
             'business'=> BusinessResource::make($this->business)
         ];
     }
