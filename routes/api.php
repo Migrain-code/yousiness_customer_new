76018fb65ad4f0e56ef200704791025e2ecef491
migrain.code <migrain.code@gmail.com>
2023-08-10 15:37:42 +0300

    hesap silme endpointi eklendi

--- routes/api.php
+++ routes/api.php
@@ -46,7 +46,7 @@ Route::prefix('customer')->group(function (){
     Route::post('update/password', [CustomerController::class, 'updatePassword']);
     Route::post('update/image', [CustomerController::class, 'updateImage']);
     Route::post('update/notify', [CustomerController::class, 'updateNotify']);
-    Route::post('delete', [CustomerController::class, 'destroy']);
+    Route::get('delete', [CustomerController::class, 'destroy']);
 });
 Route::prefix('appointment')->group(function (){
     Route::post('/business/get', [AppointmentController::class, 'businessGet']);
