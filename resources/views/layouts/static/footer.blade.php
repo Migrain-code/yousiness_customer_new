fb5a1f155223d5154c1dfa6d68b10b9850d3f798
berkcanakdeniz <berkcan@Berkcan-MacBook-Pro.local>
2023-08-21 17:48:28 +0300

    header fixed & footer design change

--- resources/views/layouts/static/footer.blade.php
+++ resources/views/layouts/static/footer.blade.php
@@ -13,32 +13,14 @@
                             <p>{{setting('footer_about')}}</p>
                         </div>
                         <div class="footer-widget footer-menu footer-menu-five">
-                            <ul>
+                            <!-- <ul>
                                 <li><a href="{{setting('speed_facebook_url')}}" target="_blank"><i class="fab fa-facebook"></i> Facebook</a>
                                 </li>
                                 <li><a href="{{setting('speed_twitter_url')}}" target="_blank"><i class="fab fa-twitter"></i> Twitter</a>
                                 </li>
                                 <li><a href="{{setting('speed_intagram_url')}}" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>
                                 </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /Footer Widget -->
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <!-- Footer Widget -->
-                    <div class="footer-widget footer-contact">
-                        <h2 class="footer-title">Kommunikation</h2>
-                        <div class="footer-contact-info">
-                            <div class="footer-address"><span><i class="feather-map-pin"></i></span>
-                                <p>{{setting('speed_contact_address')}}</p>
-                            </div>
-                            <p><i class="feather-phone"></i>
-                                {{setting('speed_contact_phone')}}</p>
-                            <p class="mb-0">
-                                <i class="feather-mail"></i>
-                                <span>{{setting('speed_contact_email')}}</span>
-                            </p>
+                            </ul> -->
                         </div>
                     </div>
                     <!-- /Footer Widget -->
@@ -65,9 +47,28 @@
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <!-- Footer Widget -->
+                    <div class="footer-widget footer-contact">
+                        <h2 class="footer-title">Kommunikation</h2>
+                        <div class="footer-contact-info">
+                            <div class="footer-address"><span><i class="feather-map-pin"></i></span>
+                                <p>{{setting('speed_contact_address')}}</p>
+                            </div>
+                            <p><i class="feather-phone"></i>
+                                {{setting('speed_contact_phone')}}</p>
+                            <p class="mb-0">
+                                <i class="feather-mail"></i>
+                                <span>{{setting('speed_contact_email')}}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <!-- /Footer Widget -->
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <!-- Footer Widget -->
                     <div class="footer-widget footer-menu">
-                        <h2 class="footer-title">Empfohlene Links</h2>
-                        <ul>
+                        <h2 class="footer-title">Social Media Links</h2>
+                        <!-- <ul>
                             @forelse($recommendedLinks as $link)
                                  <li>
                                      <a href="{{$link->url}}">{{$link->title}}</a>
@@ -75,7 +76,15 @@
                             @empty
                             @endforelse
 
-                        </ul>
+                        </ul> -->
+                        <ul>
+                                <li><a href="{{setting('speed_facebook_url')}}" target="_blank"><i class="fab fa-facebook"></i> Facebook</a>
+                                </li>
+                                <li><a href="{{setting('speed_twitter_url')}}" target="_blank"><i class="fab fa-twitter"></i> Twitter</a>
+                                </li>
+                                <li><a href="{{setting('speed_intagram_url')}}" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>
+                                </li>
+                            </ul>
                     </div>
                     <!-- /Footer Widget -->
                 </div>
