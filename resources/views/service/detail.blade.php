7a5cc18edbd1538c0a03d65bec02a227124b5c68
migrain.code <migrain.code@gmail.com>
2023-09-26 14:58:17 +0300

    -- hizmet arama personel kontrolü giderildi

--- resources/views/service/detail.blade.php
+++ resources/views/service/detail.blade.php
@@ -116,8 +116,8 @@
                 <div class="col-md-12 col-lg-8 col-xl-9">
                     <!-- Business Widget -->
                          @forelse($businesses as $business)
-                             @if($business->business)
-                            <div class="row widget-container bg-white position-relative @if(!$loop->first) my-3 @endif align-items-center rounded-3" style="box-shadow: 1px 3px 15px #00000036;cursor: pointer">
+                             @if($business->business && $business->business()->has("personel") && $business->business->city != null)
+                                <div class="row widget-container bg-white position-relative @if(!$loop->first) my-3 @endif align-items-center rounded-3" style="box-shadow: 1px 3px 15px #00000036;cursor: pointer">
                                 @if(auth('customer')->check())
                                     <a href="javascript:void(0)" data-toggle="tooltip" data-toggle="popover" title="Favorilere Ekle"  class="@if(in_array($business->business->id, $favoriteIds)) fav-btn-active @else fav-btn @endif addFav" b_id="{{$business->business->id}}">
                                         <i class="far fa-bookmark"></i>
@@ -173,8 +173,7 @@
                                     @endif
                                 </div>
                             </div>
-
-                        @endif
+                             @endif
                          @empty
                             <div class="alert alert-danger text-center aos" data-aos="fade-down">
                                 Aradığınız türde hizmet veren işletme kaydı bulunamadı.
