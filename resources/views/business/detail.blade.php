30cd786990b78bbf6dcfe20eab28920c0f0e54a1
migrain.code <migrain.code@gmail.com>
2023-12-05 10:36:25 +0300

    işletme detay test

--- resources/views/business/detail.blade.php
+++ resources/views/business/detail.blade.php
@@ -142,7 +142,7 @@
                                 <li class="tab-titles p-3 active" onclick="tabsFunction(this)">Frau</li>
                             @endif
                         </ul>
-                        {{dd($business->type->id == 3 && $womanServiceCategories->count() > $manServiceCategories->count())}}
+                        {{dd("wc: ".$womanServiceCategories->count(). "mc:". $manServiceCategories->count())}}
                         <div class="tab-contents">
                             <div class="tab-content @if($business->type->id == 3 && $manServiceCategories->count() > $womanServiceCategories->count()) active @endif">
                                 @forelse($manServiceCategories as $manCategories)
