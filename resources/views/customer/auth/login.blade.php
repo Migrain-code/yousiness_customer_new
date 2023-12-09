15af36b9f01c4ad4817af19dc66df3bd7a4ebc09
migrain.code <migrain.code@gmail.com>
2023-12-09 21:22:17 +0300

    işletme detay test

--- resources/views/customer/auth/login.blade.php
+++ resources/views/customer/auth/login.blade.php
@@ -74,13 +74,30 @@
 
     <script>
         //$(".phone").inputmask({"mask": "+99 (999)-999-9999"});
-        //$(".phone").inputmask({"mask": "(999)-999-9999"});
+
         const input = document.querySelector("#phone");
         const iti = window.intlTelInput(input, {
-            // tercihlerinize göre opsiyonları ayarlayabilirsiniz
-            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js", // numara formatlama ve doğrulama için gereklidir
+            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
         });
 
+        // Inputmask
+        $(".phone").inputmask({
+            mask: iti.getSelectedCountryData().dialCode + " (999)-999-9999",
+            showMaskOnHover: false,
+            showMaskOnFocus: false,
+        });
+
+        // Event listener for country change
+        iti.promise.then(function () {
+            iti.listen("countrychange", function () {
+                $("#phone").inputmask("remove");
+                $("#phone").inputmask({
+                    mask: iti.getSelectedCountryData().dialCode + " (999)-999-9999",
+                    showMaskOnHover: false,
+                    showMaskOnFocus: false,
+                });
+            });
+        });
         // Örnek olarak: Numarayı uluslararası formatta alma
         function getNumber() {
             return iti.getNumber();
