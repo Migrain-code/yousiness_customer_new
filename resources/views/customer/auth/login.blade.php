c42e596b81c2b64c7aae639a5efdec2c2cb50ed7
migrain.code <migrain.code@gmail.com>
2023-12-09 21:30:12 +0300

    işletme detay test

--- resources/views/customer/auth/login.blade.php
+++ resources/views/customer/auth/login.blade.php
@@ -74,7 +74,7 @@
 
     <script>
         $(document).ready(function () {
-            const input = document.querySelector("#yourPhoneNumberInput");
+            const input = document.querySelector("#phone");
             const iti = window.intlTelInput(input, {
                 utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
             });
