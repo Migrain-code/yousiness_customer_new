793d5ae242a91da77d406213ca0de1da81f1de0f
migrain.code <migrain.code@gmail.com>
2023-12-28 14:07:31 +0300

    -- şehirlere ülke kodu eklendi

--- resources/views/service/search.blade.php
+++ resources/views/service/search.blade.php
@@ -438,7 +438,7 @@
                                                     <i class="far fa-comment" style="margin-left: 15px"></i> {{$business->comments->count()}} Yorum
                                             </span>
                                     </div>
-                                    <p class="doc-location mt-1"><i class="fas fa-map-marker-alt"></i> {{isset($business->cities) ? $business->cities->name. ", ". $business->cities->post_code ." / ". $business->cities->country->name : ""}}</p>
+                                    <p class="doc-location mt-1"><i class="fas fa-map-marker-alt"></i> {{isset($business->cities) ? $business->cities->post_code. ", ". $business->cities->name ." / ". $business->cities->country->name : ""}}</p>
                                    {{--  <div class="alert alert-success py-1 mt-1">{{setting('speed_site_title')}} %10 İndirim </div> --}}
 
                                     <div class="single-comment mt-1">
@@ -449,8 +449,6 @@
                                 </div>
                             </div>
                             <div class="col-lg-2 col-sm-12 widget-footer">
-
-
                                     <div>
                                     <span style="font-size: 25px;color: #ff8a00; font-weight: bold;">{{$business->services->min("price")}}</span>
                                         Ab €
